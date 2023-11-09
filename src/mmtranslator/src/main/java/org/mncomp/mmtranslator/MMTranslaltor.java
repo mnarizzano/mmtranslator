20dfb6c6891013f2b6f5b111478c3be05898ab0c
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-09 13:28:01 +0100

    Update MMTranslaltor.java

--- src/mmtranslator/src/main/java/org/mncomp/mmtranslator/MMTranslaltor.java
+++ src/mmtranslator/src/main/java/org/mncomp/mmtranslator/MMTranslaltor.java
@@ -1,13 +1,34 @@
 package org.mncomp.mmtranslator;
-
-/**
- * Hello world!
- *
- */
-public class App 
+import org.mncomp.mmtranslator.MM.MM;
+import org.mncomp.mmtranslator.State.State;
+import org.mncomp.mmtranslator.Transition.Transition;
+import org.mncomp.mmtranslator.Signal.Signal;
+import org.mncomp.mmtranslator.Kiss2Writer.Kiss2Writer;
+import org.mncomp.mmtranslator.DotParser.DotParser;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+public class MMTranslaltor
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) throws IOException {
+        MM mealy = new MM();
+        DotParser dp = new DotParser("/Users/fateme/Documents/mmtranslator/src/mmtranslator/src/dot0.dot",mealy);
+        Kiss2Writer kw = new Kiss2Writer("/Users/fateme/Documents/mmtranslator/src/mmtranslator/src/bbara.kiss2",mealy);
+
+        try {
+            dp.parseDotFile("/Users/fateme/Documents/mmtranslator/src/mmtranslator/src/dot0.dot");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        kw.writeKiss2(mealy);
+     //   kw.closeKiss2File();
+        dp.getMealyMachine();
+        dp.closeDotFile();
+
+
+
+
     }
 }
