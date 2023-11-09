36e6cd97c309c96fd9b39055afd2bf3e0684313e
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-09 13:27:40 +0100

    Update Transition.java

--- src/mmtranslator/src/main/java/org/mncomp/mmtranslator/Transition/Transition.java
+++ src/mmtranslator/src/main/java/org/mncomp/mmtranslator/Transition/Transition.java
@@ -7,110 +7,142 @@
  *
  */
 
-package package org.mncomp.mmtranslator;
-
-import main.java.org.mncorp.State;
-
-/**
- * This Define all the methods that are necessary to store  the Transition details of a MM.
- * @see ... for more details
- * @author mnarizzano
- *
- */
+package org.mncomp.mmtranslator.Transition;
+import org.mncomp.mmtranslator.State.State;
+import java.util.concurrent.atomic.AtomicInteger;
 public class Transition {
-    
-    private static final AtomicInteger count = new AtomicInteger(0);  /* Counter for Unique Name of the Transition*/
-    private final int id;                                             /* Unique id of the transition. Fianl because can not be modified*/
-    private State stateTo;                                                 /* Arrival State of the Transition. */
-    private State stateFrom;                                               /* Starting State of the Transition. */
-    private Signal[] inputSignals;                                    /* Set of input signals*/
-    private Signal[] outputSignals;                                   /* Set of output signals*/
-
-    
-    /**
-     * Default Constructor
-     */
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private final int id;
+    private State stateTo;
+    private State stateFrom;
+    private String inputSignals;
+    private String outputSignals;
     public Transition() {
-	id = count.incrementAndGet();
+        id = count.incrementAndGet();
     }
-	
-    /**
-     * Constractor with param.
-     * 
-     * @param from, starting state of the Transition.
-     * @param to, arrival state of the transition
-     * @param InputSignals, rapresent the set of assigned input signals 
-     * @param InputSignals, rapresent the set of assigned output signals 
-     */
-    public Transition(State from, State to, Signal[] inputSignals, Signal[] outputSignals){
-	this.Transition();
-	setStateFrom(from);
-	setStateTo(to);
-	setInputSignals(inputSignals);
-	setOutputSignals(outputSignals);
+    public Transition(State from, State to, String[] inputSignals, String[] outputSignals) {
+        this();
+        setStateFrom(from);
+        setStateTo(to);
+        setInputSignals(inputSignals[0]);
+        setOutputSignals(outputSignals[0]);
     }
 
-    /**
-     * 
-     * @return the id(name)  of the transition
-     */
-    int getId(){ return id;}
-
-    /**
-     * @return the arrival state of the transition
-     */
-    State getStateTo(){
-	return stateTo;
+    public int getId() {
+        return id;
     }
-
-    /**
-     * @return the starting state of the transition
-     */
-    State getStateFrom(){
-	return stateFrom;
+    public State getStateTo() {
+        return stateTo;
     }
-
-    /**
-     * @return return the set of assigned input signals 
-     */
-    Signal[] getInputSignals(){
-	return inputSignals;
+    public State getStateFrom() {
+        return stateFrom;
     }
-
-        /**
-     * @return return the set of assigned output signals 
-     */
-    Signal[] getOutputSignals(){
-	return outputSignals;
+    public String getInputSignals() {
+        return inputSignals;
     }
-
-
- 
-    /**
-     * @param to, is the arrival state of the transition
-     */
-    void setStateTo(State to){
-	stateTo = to;
+    public String getOutputSignals() {
+        return outputSignals;
     }
-
-    /**
-     * @param from, the starting state of the transition
-     */
-    void setStateFrom(State from){
+    public void setStateTo(State to) {
+        stateTo = to;
+    }
+    public void setStateFrom(State from) {
         stateFrom = from;
     }
-
-    /**
-     * @param input, a set of assigned input signals 
-     */
-    void setInputSignals(Signal[] input){
-	inputSignals = input.clone();
+    public void setInputSignals(String input) {
+        inputSignals = input;
     }
-    
-    /**
-     * @param output, a set of assigned output signals 
-     */
-    void setOutputSignals(Signal[] output){
-	outputSignals = output.clone();
+    public void setOutputSignals(String output) {
+        outputSignals = output;
     }
+}
 
+// public class Transition {
+
+//     private static final AtomicInteger count = new AtomicInteger(0);  /* Counter for Unique Name of the Transition*/
+//     private final int id;                                             /* Unique id of the transition. Fianl because can not be modified*/
+//     private State stateTo;                                                 /* Arrival State of the Transition. */
+//     private State stateFrom;                                               /* Starting State of the Transition. */
+//     private Signal[] inputSignals;                                    /* Set of input signals*/
+//     private Signal[] outputSignals;                                   /* Set of output signals*/
+
+
+//     /**
+//      * Default Constructor
+//      */
+//     public Transition() {
+//         id = count.incrementAndGet();
+//     }
+
+//     /**
+//      * Constractor with param.
+//      *
+//      * @param from,         starting state of the Transition.
+//      * @param to,           arrival state of the transition
+//      * @param InputSignals, rapresent the set of assigned input signals
+//      * @param InputSignals, rapresent the set of assigned output signals
+//      */
+//     public Transition(State from, State to, Signal[] inputSignals, Signal[] outputSignals) {
+//         this();
+//         setStateFrom(from);
+//         setStateTo(to);
+//         setInputSignals(inputSignals);
+//         setOutputSignals(outputSignals);
+//     }
+
+//     /**
+//      * @return the id(name)  of the transition
+//      */
+//     int getId() {
+//         return id;
+//     }
+
+//     /**
+//      * @return the arrival state of the transition
+//      */
+//     State getStateTo() {
+//         return stateTo;
+//     }
+
+//     /**
+//      * @return the starting state of the transition
+//      */
+//     State getStateFrom() {
+//         return stateFrom;
+//     }
+
+//     /**
+//      * @return return the set of assigned input signals
+//      */
+//     Signal[] getInputSignals() {
+//         return inputSignals;
+//     }
+
+//     /**
+//      * @return return the set of assigned output signals
+//      */
+//     Signal[] getOutputSignals() {
+//         return outputSignals;
+//     }
+
+
+//     /**
+//      * @param to, is the arrival state of the transition
+//      */
+//     void setStateTo(State to) {
+//         stateTo = to;
+//     }
+
+//     /**
+//      * @param from, the starting state of the transition
+//      */
+//     void setStateFrom(State from) {
+//         stateFrom = from;
+//     }
+
+//     /**
+//      * @param input, a set of assigned input signals
+//      */
+//     void setInputSignals(Signal[] input) {
+//         inputSignals = input.clone();
+//     }
