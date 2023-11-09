dec6430f4e180a540a7495f32d7af14f8f390d9a
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-09 13:30:23 +0100

    Update Signal.java

--- src/mmtranslator/src/main/java/org/mncomp/mmtranslator/Signal/Signal.java
+++ src/mmtranslator/src/main/java/org/mncomp/mmtranslator/Signal/Signal.java
@@ -1,50 +1,48 @@
-package org.mncomp.mmtranslator.Signal; /**
- * This file contains the definition of the variables that are added
- * to each transition/mealy machine. A variables is called Signal, and
- * other than names it has also a value defining if it is positive
- * signal or a negative signal.
- * @author mnarizzano
- *
- */
+// package org.mncomp.mmtranslator.Signal; /**
+//  * This file contains the definition of the variables that are added
+//  * to each transition/mealy machine. A variables is called Signal, and
+//  * other than names it has also a value defining if it is positive
+//  * signal or a negative signal.
+//  * @author mnarizzano
+//  *
+//  */
 
-import java.util.*;
-import java.*;
+// import java.util.*;
+// import java.*;
 
-/**
- * This Define all the methods that are necessary to store the Signal
- * labelling Transitions
- * @see ... for more details
- * @author mnarizzano
- *
- */
+// /**
+//  * This Define all the methods that are necessary to store the Signal
+//  * labelling Transitions
+//  * @see ... for more details
+//  * @author mnarizzano
+//  *
+//  */
+// public class Signal {
+//     private String name;
+//     private int value;
+
+// }
+package org.mncomp.mmtranslator.Signal;
 public class Signal {
     private String name;
-    private SignalType type;
-
-    public Signal(String name, SignalType type) {
+    private int value;
+    public Signal(String name, int value) {
         this.name = name;
-        this.type = type;
+        this.value = value;
     }
-
-    // Getter and Setter methods for name and type
-    public String getName() {
+    public Signal(String name){
+        this.name = name;
+    }
+    public String getSignalName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public SignalType getType() {
-        return type;
+    public int getValue() {
+        return value;
     }
-
-    public void setType(SignalType type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "Signal [name=" + name + ", type=" + type + "]";
+    public void setValue(int value) {
+        this.value = value;
     }
 }
