fcc24a116291bc7f624cf141864bb0af21ce0b70
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-28 20:33:02 +0100

    Update State.java

--- src/mmtranslator/src/main/java/org/mncomp/mmtranslator/State/State.java
+++ src/mmtranslator/src/main/java/org/mncomp/mmtranslator/State/State.java
@@ -1,79 +1,61 @@
-/**
- * This file contains the definition of the State class 
- * @author mnarizzano
- *
- */
+package org.mncomp.mmtranslator.State;
 
-package package org.mncomp.mmtranslator;
-
-import main.java.org.mncorp.MM;
-
-/**
- * This Define all the methods that are necessary to store  the state details of a MM.
- * @see ... for more details
- * @author mnarizzano
- *
- */
 public class State {
-    private String name;       /* Name of the State*/
-    private Bool   init;   /* Indicate if the state is an initial state (True) or not(False)*/
+    // Name of the State
+    private String name;
 
-    /**
-     * Default Constructor
-     */
-    public State() {}
-	
-    /**
-     * Constractor with param.
-     * 
-     * @param name, the name of the state.
-     * @param init, indicate that the state is of init. Is a boolean value, if True the state is an initial state.
-     */
-    public State(String name, bool init) { 
-	setName(name);
-	if (init) {
-	    setInit();
-	} else {
-	    unsetInit();
-	}
+    // Indicates if the state is an initial state (True) or not (False)
+    private boolean init;
+
+    // State number (not currently used in the provided code)
+    private int statenumber;
+
+    // Unique identifier for the state
+    private int id;
+
+    // Default constructor (not currently used in the provided code)
+    public State() {
+        // Default constructor
     }
 
-    /**
-     * Set the name of the State Object 
-     * @param name, name of the state
-     */
-    void setName(String Name){}
-
-    /**
-     * Get the name of the State Object 
-     * @return the name of the state
-     */
-    String getName(){
-	return name;
+    // Parameterized constructor to set the state name and ID
+    public State(String name, int id) {
+        this.name = name;
+        this.id = id;
     }
 
-
-    /**
-     * Set the state as an initial State
-     */
-    void setInitial(){
-	init = true;
+    // Getter method to retrieve the name of the state
+    public String getStateName() {
+        return name;
     }
 
-    /**
-     * Set the state as not initial. 
-     */
-    String unsetInitial(){
-	init = false;
+    // Getter method to retrieve the unique identifier of the state
+    public int getId() {
+        return id;
     }
 
-    /**
-     * Ask if the State is inisial.
-     * @return True if the state is initial, False otherwise 
-     */
-    Bool isInitial(){
-	return init;
+    // Getter method to retrieve the state number (not currently used)
+    public int getStateNo() {
+        return statenumber;
     }
 
-    
+    // Setter method to set the name of the state
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method to check if the state is an initial state
+    public boolean isInit() {
+        return init;
+    }
+
+    // Setter method to set the state as an initial state
+    public void setInit() {
+        init = true;
+    }
+
+    // Setter method to set the state as not an initial state
+    public void unsetInit() {
+        init = false;
+    }
 }
