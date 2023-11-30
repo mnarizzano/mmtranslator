cd6a704eb59613c31719f37dfcc552fe470147f5
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-30 02:14:18 +0100

    Update SignalTest.java

--- src/test/java/org/mncomp/mmtranslator/Signal/SignalTest.java
+++ src/test/java/org/mncomp/mmtranslator/Signal/SignalTest.java
@@ -1,19 +1,39 @@
+// Package declaration for the test class
 package org.mncomp.mmtranslator.Signal;
+
+// Import statements for required classes and methods
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Signal class.
+ */
 class SignalTest {
 
+    /**
+     * Test the getSignalName method of the Signal class.
+     */
     @Test
     void getSignalName() {
+        // Arrange: Create a Signal instance with a specific name
         Signal signal = new Signal("ExampleSignal");
+
+        // Act: Call the getSignalName method and check the result
         assertEquals("ExampleSignal", signal.getSignalName());
     }
 
+    /**
+     * Test the setName method of the Signal class.
+     */
     @Test
     void setName() {
+        // Arrange: Create a Signal instance with an initial name
         Signal signal = new Signal("InitialSignal");
+
+        // Act: Call the setName method to update the name
         signal.setName("UpdatedSignal");
+
+        // Assert: Check if the name has been updated
         assertEquals("UpdatedSignal", signal.getSignalName());
     }
 }
