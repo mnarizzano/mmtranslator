e55ef051317924a73508e841ee6a7e7b46c48fcf
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-30 02:08:53 +0100

    Update MMTranslator.java

--- src/main/java/org/mncomp/mmtranslator/MMTranslator.java
+++ src/main/java/org/mncomp/mmtranslator/MMTranslator.java
@@ -1,3 +1,12 @@
+**
+ * This file contains the definition of the MMTranslator class.
+ * The MMTranslator class is the entry point for the Mealy Machine translation process.
+ * It creates instances of the Mealy Machine, DotParser, and Kiss2Writer to parse Dot files
+ * and generate Kiss2 files based on the provided Mealy Machine.
+ * 
+ * @author RominaAdhami
+ */
+
 package org.mncomp.mmtranslator;
 
 import org.mncomp.mmtranslator.MM.MM;
@@ -8,30 +17,39 @@ import java.io.IOException;
 
 public class MMTranslator {
     public static void main(String[] args) throws IOException {
-        // Create an instance of Mealy Machine
+        // Create an instance of the Mealy Machine
         MM mm = new MM();
-        // Create a DotParser instance with the file path and the Mealy Machine instance
-        DotParser dp = new DotParser("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/dot0.dot", mm);
-        // Create a Kiss2Writer instance with the file path and the Mealy Machine instance
-        Kiss2Writer kw = new Kiss2Writer("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/bbara.kiss2", mm);
+
+        // Create a DotParser instance with the Dot file path and the Mealy Machine instance
+        DotParser dotParser = new DotParser("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/dot0.dot", mm);
+
+        // Create a Kiss2Writer instance with the Kiss2 file path and the Mealy Machine instance
+        Kiss2Writer kiss2Writer = new Kiss2Writer("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/bbara.kiss2", mm);
+
         try {
             // Parse the Dot file to populate the Mealy Machine
-            dp.parseDotFile("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/dot0.dot");
+            dotParser.parseDotFile("/Users/fateme/Downloads/mmtranslator 3/src/mmtranslator/src/dot0.dot");
         } catch (IOException e) {
             // Handle IOException gracefully or log the error
             throw new RuntimeException(e);
         }
+
         // Print information about the Mealy Machine
         System.out.println("Input Signals: " + mm.getInputSignals().size());
         System.out.println("Output Signals: " + mm.getOutputSignals().size());
         System.out.println("Number of States: " + mm.getAllStates().size());
         System.out.println("Number of Transitions: " + mm.getAllTransitions().size());
+
         // Write the Mealy Machine to the Kiss2 file
-        kw.writeKiss2(mm);
-        // Write the Mealy Machine to the Kiss2 file
-        kw.closeKiss2File();
-        dp.getMealyMachine();
+        kiss2Writer.writeKiss2(mm);
+
+        // Close the Kiss2 file
+        kiss2Writer.closeKiss2File();
+
+        // Retrieve the Mealy Machine from the DotParser (not currently used in the provided code)
+        dotParser.getMealyMachine();
+
         // Close the Dot file
-        dp.closeDotFile();
+        dotParser.closeDotFile();
     }
 }
