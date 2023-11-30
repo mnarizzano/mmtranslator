9b315dc469a08f5e176d2f6a16e6c2d60869cfef
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-30 01:56:38 +0100

    Update State.java

--- src/main/java/org/mncomp/mmtranslator/State/State.java
+++ src/main/java/org/mncomp/mmtranslator/State/State.java
@@ -1,5 +1,14 @@
+/**
+ * This file contains the definition of the State class.
+ *
+ * @author RominaAdhami
+ */
 package org.mncomp.mmtranslator.State;
 
+/**
+ * This class defines all the methods necessary to store the state details of a Mealy Machine.
+ * 
+ */
 public class State {
     // Name of the State
     private String name;
@@ -13,48 +22,80 @@ public class State {
     // Unique identifier for the state
     private int id;
 
-    // Default constructor (not currently used in the provided code)
+    /**
+     * Default Constructor
+     */
     public State() {
         // Default constructor
     }
 
-    // Parameterized constructor to set the state name and ID
+      /**
+     * Constructor with parameters.
+     *
+     * @param name Name of the state.
+     * @param init Indicates whether the state is an initial state. 
+     *             If true, the state is an initial state; otherwise, it is not.
+     */
     public State(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
-    // Getter method to retrieve the name of the state
+     /**
+     * Getter method to retrieve the name of the state.
+     *
+     * @return The name of the state.
+     */
     public String getStateName() {
         return name;
     }
 
-    // Getter method to retrieve the unique identifier of the state
+    /**
+     * Getter method to retrieve the unique identifier of the state.
+     *
+     * @return The unique identifier of the state.
+     */
     public int getId() {
         return id;
     }
-
-    // Getter method to retrieve the state number (not currently used)
+    
+   /**
+     * Getter method to retrieve the state number (not currently used).
+     *
+     * @return The state number.
+     */
     public int getStateNo() {
         return statenumber;
     }
 
-    // Setter method to set the name of the state
+    /**
+     * Setter method to set the name of the state.
+     *
+     * @param name Name to set for the state.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter method to check if the state is an initial state
+     /**
+     * Getter method to check if the state is an initial state.
+     *
+     * @return True if the state is an initial state, false otherwise.
+     */
     public boolean isInit() {
         return init;
     }
 
-    // Setter method to set the state as an initial state
+    /**
+     * Setter method to set the state as an initial state.
+     */
     public void setInit() {
         init = true;
     }
 
-    // Setter method to set the state as not an initial state
+    /**
+     * Setter method to set the state as not an initial state.
+     */
     public void unsetInit() {
         init = false;
     }
