c4a1da09da0a219214e33af71e477787e5c5064a
Romina Adhmai <102147200+rominaadhami@users.noreply.github.com>
2023-11-30 02:16:34 +0100

    Update AppTest.java

--- src/test/java/org/mncomp/mmtranslator/AppTest.java
+++ src/test/java/org/mncomp/mmtranslator/AppTest.java
@@ -1,20 +1,27 @@
+// Package declaration for the test class
 package org.mncomp.mmtranslator;
 
+// Import statement for the required method
 import static org.junit.Assert.assertTrue;
 
+// Import statement for the JUnit test annotation
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit tests for the App class.
  */
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
+     * This test method checks a simple assertion to ensure the testing environment is functioning.
+     * It verifies that the boolean expression `assertTrue(true)` holds true.
      */
     @Test
     public void shouldAnswerWithTrue()
     {
+        // Act: Call the assertTrue method with the argument true
         assertTrue( true );
     }
 }
+
