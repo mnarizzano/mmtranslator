4e53104132e435f5b75e20e1a0472f0f4768d6d6
fateme ozgoli <fatemeozgoli@gmail.com>
2024-06-27 16:38:00 +0200

    Update SignalTest.java

--- src/test/java/org/mncomp/mmtranslator/Signal/SignalTest.java
+++ src/test/java/org/mncomp/mmtranslator/Signal/SignalTest.java
@@ -5,15 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Signal class.
+ */
 public class SignalTest {
 
     private Signal signal;
 
+    /**
+     * Sets up the test environment before each test.
+     */
     @BeforeEach
     void setUp() {
         signal = new Signal("TestSignal", 42);
     }
 
+    /**
+     * Tests the constructor with value.
+     */
     @Test
     void testConstructorWithValue() {
         assertNotNull(signal);
@@ -21,6 +30,9 @@ public class SignalTest {
         assertEquals(42, signal.getValue());
     }
 
+    /**
+     * Tests the constructor without value.
+     */
     @Test
     void testConstructorWithoutValue() {
         Signal signalWithoutValue = new Signal("SignalWithoutValue");
@@ -29,22 +41,34 @@ public class SignalTest {
         assertEquals(0, signalWithoutValue.getValue()); // Default value should be 0
     }
 
+    /**
+     * Tests the getSignalName method.
+     */
     @Test
     void testGetSignalName() {
         assertEquals("TestSignal", signal.getSignalName());
     }
 
+    /**
+     * Tests the setName method.
+     */
     @Test
     void testSetName() {
         signal.setName("NewName");
         assertEquals("NewName", signal.getSignalName());
     }
 
+    /**
+     * Tests the getValue method.
+     */
     @Test
     void testGetValue() {
         assertEquals(42, signal.getValue());
     }
 
+    /**
+     * Tests the setValue method.
+     */
     @Test
     void testSetValue() {
         signal.setValue(100);
