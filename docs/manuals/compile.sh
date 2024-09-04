9cf13fff5f3fa326882802809a21d8ee9b5dd33e
fateme ozgoli <fatemeozgoli@gmail.com>
2024-09-04 20:54:40 +0200

    Run_and_Compile.sh

--- docs/manuals/compile.sh
+++ docs/manuals/compile.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Set the root directory of your project
-PROJECT_DIR="/home/west/Desktop/fatemeh/mmtranslator"
+# Create the output directory if it doesn't exist
+mkdir -p out
 
-# Create the target directory if it doesn't exist
-mkdir -p "$PROJECT_DIR/target"
+# Compile all Java files in the src/main/java directory
+javac -d out $(find src/main/java -name "*.java")
 
-# Compile the source code and place the compiled classes in the target directory
-find "$PROJECT_DIR/src/main/java" -name "*.java" -print | xargs javac -d "$PROJECT_DIR/target" -cp "$PROJECT_DIR/src/main/java"
-
-# Compile the source code
-javac -d target -cp src/main/java src/main/java/org/mncomp/mmtranslator/MMTranslator.java
-
-# Create the jar file
-jar cfe MMTranslator.jar org.mncomp.mmtranslator.MMTranslator -C target .
+# Run the MMTranslator class
+java -cp out org.mncomp.mmtranslator.MMTranslator
